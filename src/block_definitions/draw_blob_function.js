9981ff0d979b0f9210f64f5f3807f66cfa8f637b
ivyxuan <iloveivyxuan@gmail.com>
2020-10-19 22:38:23 -0500

    Fix blob blocks

--- src/block_definitions/draw_blob_function.js
+++ src/block_definitions/draw_blob_function.js
@@ -6,17 +6,13 @@ Blockly.Blocks['draw_blob'] = {
         .appendField("Draw a Blob");
     this.appendValueInput("radius")
         .setCheck(null)
-        .appendField("Radius");
-    this.appendValueInput("x")
-        .setCheck(null)
-        .appendField("X axis");
-    this.appendValueInput("y")
-        .setCheck(null)
-        .appendField("Y axis");
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("radius");
     this.appendValueInput("color")
         .setCheck(null)
-        .appendField("Color");
-    this.setColour(150);
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("color");
+    this.setColour(50);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setTooltip("");
@@ -29,20 +25,20 @@ Blockly.JavaScript['draw_blob'] = function(block) {
   var value_x = Blockly.JavaScript.valueToCode(block, 'x', Blockly.JavaScript.ORDER_ATOMIC);
   var value_y = Blockly.JavaScript.valueToCode(block, 'y', Blockly.JavaScript.ORDER_ATOMIC);
   var value_color = Blockly.JavaScript.valueToCode(block, 'color', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'draw_blob('+value_radius+','+value_x+','+value_y+', '+value_color+');\n';
+  var code = 'draw_blob('+value_radius+','+Math.floor(Math.random()*5)+','+Math.floor(Math.random()*5)+', '+value_color+', '+Math.floor(Math.random()*5)+');\n';
   return code;
 };
 
-function draw_blob(size, xFactor, yFactor, color) {
+function draw_blob(size, xFactor, yFactor, color, diff) {
   const xCenter = width/2 + noise(t/2 + xFactor) * 200 - 100;
   const yCenter = height/2 + noise(t/2 + yFactor) * 200 - 100;
   fill(`${color}`);
   beginShape();
   for (let theta = 0; theta < 2 * PI; theta += 0.01) {
     let r1, r2;
-    r1 = cos(theta)+1;
-    r2 = sin(theta)+1;
-    let r = size + noise(0.75 * r1, 0.75 * r2, t) * (2/3) * size;
+    r1 = cos(theta) + 1;
+    r2 = sin(theta) + 1;
+    let r = size + noise(0.75 * r1, 0.75 * r2, t + diff) * (2/3) * size;
     let x = xCenter + r * cos(theta);
     let y = yCenter + r * sin(theta);
     curveVertex(x, y);
--- src/rectangle_tutorial.js
+++ src/rectangle_tutorial.js
@@ -53,6 +53,8 @@ Blockly.Blocks['rotate_by_degree'] = {
         .setCheck(null)
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("degree");
+    this.appendDummyInput()
+        .appendField("°");
     this.setInputsInline(true);
     this.setColour(400);
     this.setPreviousStatement(true);
@@ -90,57 +92,57 @@ document.addEventListener("DOMContentLoaded", function () {
   const lang = 'JavaScript';
   const button = document.getElementById('blocklyButton');
 
-  // var xml_text = `
-  //   <xml xmlns="https://developers.google.com/blockly/xml">
-  //     <block type="controls_repeat_ext" id="BvA4Pb(-mkwR//Wx{F*/" x="148" y="185">
-  //       <value name="TIMES">
-  //         <shadow type="math_number" id="H80E#H@WUWVT593V2J1g">
-  //           <field name="NUM">60</field>
-  //         </shadow>
-  //       </value>
-  //       <statement name="DO">
-  //         <block type="rotate_by_degree" id="8_}(QaWqOTdeq7aK]6~B">
-  //           <value name="degree">
-  //             <block type="math_number" id="=Q$h[lZEU}d^2Fj!h7,">
-  //               <field name="NUM">4</field>
-  //             </block>
-  //           </value>
-  //           <next>
-  //             <block type="draw_rectangle" id="K4#S/H~dz;G_g$51z4pV">
-  //               <value name="position_x">
-  //                 <block type="math_number" id="N:^A[SbM2]6lA;wG658">
-  //                   <field name="NUM">0</field>
-  //                 </block>
-  //               </value>
-  //               <value name="position_y">
-  //                 <block type="math_number" id="mw}j%}F:5xnJu85Zz-a~">
-  //                   <field name="NUM">0</field>
-  //                 </block>
-  //               </value>
-  //               <value name="width">
-  //                 <block type="math_number" id="nr4c7ienpG-V]|Na#!Ch">
-  //                   <field name="NUM">100</field>
-  //                 </block>
-  //               </value>
-  //               <value name="height">
-  //                 <block type="math_number" id="ri7t+ZKzUsmnNlTIXvkU">
-  //                   <field name="NUM">100</field>
-  //                 </block>
-  //               </value>
-  //               <value name="color">
-  //                 <block type="colour_picker" id="%6w^?.=[6EZRW-t573v">
-  //                   <field name="COLOUR">#993399</field>
-  //                 </block>
-  //               </value>
-  //             </block>
-  //           </next>
-  //         </block>
-  //       </statement>
-  //     </block>
-  //   </xml>
-  // `
-  // var xml = Blockly.Xml.textToDom(xml_text);
-  // Blockly.Xml.domToWorkspace(xml, workspace);
+  var xml_text = `
+    <xml xmlns="https://developers.google.com/blockly/xml">
+      <block type="controls_repeat_ext" id="BvA4Pb(-mkwR//Wx{F*/" x="148" y="185">
+        <value name="TIMES">
+          <shadow type="math_number" id="H80E#H@WUWVT593V2J1g">
+            <field name="NUM">60</field>
+          </shadow>
+        </value>
+        <statement name="DO">
+          <block type="rotate_by_degree" id="8_}(QaWqOTdeq7aK]6~B">
+            <value name="degree">
+              <block type="math_number" id="=Q$h[lZEU}d^2Fj!h7,">
+                <field name="NUM">4</field>
+              </block>
+            </value>
+            <next>
+              <block type="draw_rectangle" id="K4#S/H~dz;G_g$51z4pV">
+                <value name="position_x">
+                  <block type="math_number" id="N:^A[SbM2]6lA;wG658">
+                    <field name="NUM">0</field>
+                  </block>
+                </value>
+                <value name="position_y">
+                  <block type="math_number" id="mw}j%}F:5xnJu85Zz-a~">
+                    <field name="NUM">0</field>
+                  </block>
+                </value>
+                <value name="width">
+                  <block type="math_number" id="nr4c7ienpG-V]|Na#!Ch">
+                    <field name="NUM">100</field>
+                  </block>
+                </value>
+                <value name="height">
+                  <block type="math_number" id="ri7t+ZKzUsmnNlTIXvkU">
+                    <field name="NUM">100</field>
+                  </block>
+                </value>
+                <value name="color">
+                  <block type="colour_picker" id="%6w^?.=[6EZRW-t573v">
+                    <field name="COLOUR">#993399</field>
+                  </block>
+                </value>
+              </block>
+            </next>
+          </block>
+        </statement>
+      </block>
+    </xml>
+  `
+  var xml = Blockly.Xml.textToDom(xml_text);
+  Blockly.Xml.domToWorkspace(xml, workspace);
 
   button.addEventListener('click', function () {
     var xml = Blockly.Xml.workspaceToDom(workspace);
@@ -148,8 +150,9 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(xml_text);
     var code = Blockly[lang].workspaceToCode(workspace);
     console.log(code)
+    var geval = eval;
     try {
-      eval(`
+      geval(`
         window.draw = function() {
           background("#fff")
           translate(width/2, height/2);
