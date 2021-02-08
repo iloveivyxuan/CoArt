b58faddc3a0f7599632354d9343dafa5881c9670
ivyxuan <iloveivyxuan@gmail.com>
2021-02-07 18:01:55 -0600

    Fix express server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const express = require('express');
 
-const app = express(),
-            DIST_DIR = __dirname,
-            HTML_FILE = path.join(DIST_DIR, '/build/index.html')
+const app = express()
 
 app.use(express.static(path.join(__dirname, "build")))
 
-app.get('*', (req, res) => {
-  res.sendFile(HTML_FILE)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'))
+})
+
+app.get('/tutorial', (req, res) => {
+	res.sendFile(path.join(__dirname, '/build/tutorial.html'))
 })
 
 const PORT = process.env.PORT || 8080
