9fa7808f2cb4913991a13c5dbbba01491907bcb9
ivyxuan <iloveivyxuan@gmail.com>
2021-02-27 23:43:55 -0600

    Add artwords api sample

--- server.js
+++ server.js
@@ -17,27 +17,47 @@ app.get('/sandbox', (req, res) => {
 	res.sendFile(path.join(__dirname, '/build/sandbox.html'))
 })
 
+// Resources artworks
+app.get('/artworks', (req, res) => {
+	return res.send('GET HTTP method on artwork Resources');
+});
+
+app.get('artworks/:artworkId', (req, res) => {
+	return res.send(`GET HTTP method on artwork/${req.params.artworkId} resource`)
+});
+
+app.post('artworks', (req, res) => {
+	return res.send('POST HTTP method on artwork Resources');
+});
+
+app.put('artworks/:artworkId', (req, res) => {
+	return res.send(`PUT HTTP method on artwork/${req.params.artworkId} resource`);
+});
+
+app.delete('artwork/:artworkId', (req, res) => {
+	return res.send(`DELETE HTTP method on artwork/${req.params.artworkId} resource`);
+})
+
+// express listen at port 8080
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`App listening to ${PORT}....`)
   console.log('Press Ctrl+C to quit.')
 })
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+// const MongoClient = require('mongodb').MongoClient;
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
 
-// Database Name
-const dbName = 'creativeArtDB';
-const client = new MongoClient(url);
-// Use connect method to connect to the server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log('Connected successfully to server');
+// // Database Name
+// const dbName = 'creativeArtDB';
+// const client = new MongoClient(url);
+// // Use connect method to connect to the server
+// client.connect(function(err) {
+//   console.log('Connected successfully to server');
 
-  const db = client.db(dbName);
+//   const db = client.db(dbName);
 
-  client.close();
-});
+//   client.close();
+// });
