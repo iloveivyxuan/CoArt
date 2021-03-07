ba31abd9d9cbba09403d2a1eed3d384db9ec0b24
ivyxuan <iloveivyxuan@gmail.com>
2021-03-07 16:20:18 -0600

    Dynamically load library blocks

--- src/sandbox.js
+++ src/sandbox.js
@@ -3,64 +3,53 @@ import './css/global.css';
 import './js/canvas';
 import { firebase, database } from './js/database_init';
 
-const libraries = [];
-
-const geval = eval;
-var libraryBody = ``;
-var libraryRef = firebase.database().ref(`libraries/`);
-libraryRef.on('value', (snapshot) => {
-  const data = snapshot.val();
-  const libraries = Object.keys(data);
-  for (var libraryName of libraries) {
-    var methodsText = ``;
-    const methods = Object.keys(data[libraryName]);
-    for (var methodName of methods) {
-      const method = data[libraryName][methodName]['function'];
-      methodsText += `${methodName}: ${method},`
-    }
-    libraryBody = `
-      var ${libraryName} = {
-        ${methodsText}
-      }
-    `
-  }
-  geval(libraryBody);
-});
-
 document.addEventListener("DOMContentLoaded", function() {
-  var toolbox = document.querySelector('#toolbox').innerHTML;
-  toolbox = `
-  <xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
-    ${toolbox}
-    <category name="Library" expanded="false">
-      <category name="Polygon" colour="#EE8DB0">
-        <block type="polygon">
-          <value name="sides">
-            <shadow type="math_number">
-              <field name="NUM">3</field>
-            </shadow>
-          </value>
-          <value name="radius">
-            <shadow type="math_number">
-              <field name="NUM">50</field>
-            </shadow>
-          </value>
-        </block>
-        <block type="rounded_polygon">
-          <value name="sides">
-            <shadow type="math_number">
-              <field name="NUM">3</field>
-            </shadow>
-          </value>
-          <value name="radius">
-            <shadow type="math_number">
-              <field name="NUM">50</field>
-            </shadow>
-          </value>
-        </block>
+  var libraries = [];
+  var data;
+
+  const geval = eval;
+  var libraryBody = ``;
+  var libraryRef = firebase.database().ref(`libraries/`);
+  libraryRef.on('value', (snapshot) => {
+    data = snapshot.val();
+    libraries = Object.keys(data);
+    for (var libraryName of libraries) {
+      var methodsText = ``;
+      const methods = Object.keys(data[libraryName]);
+      for (var methodName of methods) {
+        const method = data[libraryName][methodName]['function'];
+        methodsText += `${methodName}: ${method},`
+      }
+      libraryBody = `
+        var ${libraryName} = {
+          ${methodsText}
+        }
+      `
+    }
+    geval(libraryBody);
+
+    var toolbox = document.querySelector('#toolbox').innerHTML;
+    var innerCategories = ``;
+    for (const library of libraries) {
+      var blocks = ``;
+      const methods = Object.keys(data[library]);
+      for (var method of methods) {
+        blocks += data[library][method]['funcCallBlock'];
+      }
+      innerCategories += `
+        <category name="${library}" colour="#EE8DB0">
+          ${blocks}
+        </category>
+      `;
+    }
+    toolbox = `
+    <xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
+      ${toolbox}
+      <category name="Library" expanded="false">
+        ${innerCategories}
       </category>
-    </category>
-  </xml>
-  `;
-  workspace.updateToolbox(toolbox);
+    </xml>
+    `;
+    workspace.updateToolbox(toolbox);
+  });
 });
