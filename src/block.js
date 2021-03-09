42ae2ab965195e0964f17be4ee2e5f0589b78da8
ivyxuan <iloveivyxuan@gmail.com>
2021-03-08 22:47:39 -0600

    Fix library block check

--- src/block.js
+++ src/block.js
@@ -7,11 +7,14 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const libraryName = urlParams.get('library');
 const funcName = urlParams.get('func');
+console.log(libraryName)
+console.log(funcName)
 
 document.addEventListener("DOMContentLoaded", function() {
-  var funcRef = firebase.database().ref(`library/${libraryName}/${funcName}`);
+  var funcRef = firebase.database().ref(`libraries/${libraryName}/${funcName}`);
   funcRef.on('value', (snapshot) => {
-    const blocks = snapshot.val();
+    const data = snapshot.val();
+    const blocks = data['funcDefBlock'];
     const xml = Blockly.Xml.textToDom(blocks);
     Blockly.Xml.domToWorkspace(xml, window.workspace);
   });
